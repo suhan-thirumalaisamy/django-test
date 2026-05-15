2623cc8224c5593bd187f33f2e1f98c7d996946d
Suhan <suhan.thirumalai@rootquotient.com>
2026-05-15 18:07:14 +0530

    update ssl config

--- myrevoltedjango/settings.py
+++ myrevoltedjango/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
         'HOST': os.environ.get('DATABASE_HOST', ''),
         'PORT': os.environ.get('DATABASE_PORT', '5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
